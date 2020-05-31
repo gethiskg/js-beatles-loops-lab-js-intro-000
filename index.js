7e21a56858e15cb18bbd32a531f5ccd761d836cc
Ismaiilov Atai <atai2497@gmail.com>
2020-05-31 19:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  let array = [];
-  for(var i = 0; i < musicians.length; i++){
-array.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-return array;
-}
-
-function johnLennonFacts(array) {
-  var i = 0
-  while (i < array.length) {
-    array[i] += '!!!'; i++;
-  }
-  return array;
-}
-
-
-
-function iLoveTheBeatles(number) {
-  var arr = [];
-  var i = 0;
-  do {
-    arr.push('I love the Beatles!');
-    i++;
-  } while (i <= number && number < 15);
-  return arr;
-}
+// // add solution here
+// function theBeatlesPlay(musicians, instruments) {
+//   let array = [];
+//   for(var i = 0; i < musicians.length; i++){
+// array.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+// return array;
+// }
+//
+// function johnLennonFacts(array) {
+//   var i = 0
+//   while (i < array.length) {
+//     array[i] += '!!!'; i++;
+//   }
+//   return array;
+// }
+//
+//
+//
+// function iLoveTheBeatles(number) {
+//   var arr = [];
+//   var i = 0;
+//   do {
+//     arr.push('I love the Beatles!');
+//     i++;
+//   } while (i <= number && number < 15);
+//   return arr;
+// }
+// function theBeatlesPlay(musicians, instruments){
+//   var oneOne = [];
+//   for (var i = 0; i < musicians.length; i++) {
+//     oneOne.push(`${musicians[i]} plays ${instruments[i]}`)
+//   }
+//   return oneOne;
+// }
+// function johnLennonFacts(array) {
+// var i = 0
+// while (i < array.length) {
+//   array[i] += `!!!`; i++;
+// }
+// return array;
+// }
+//
+// function iLoveTheBeatles(number) {
+//   var stores = [];
+//   var i = 0;
+//   do {
+//     stores.push(`I love the Beatles!`)
+//     i++;
+//   } while (i <= number &&  number <= 15)
+//   return stores;
+// }
